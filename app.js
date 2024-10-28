ac04274ce16c25b7deaf1b0c04ea6b4c4672ccd7
CrysParteka <162991031+CrysParteka@users.noreply.github.com>
2024-10-28 09:01:52 -0300

    vv

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 function criaCartao(categoria, pergunta, resposta) {
-    console.log(categoria, pergunta, resposta)
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
 }
