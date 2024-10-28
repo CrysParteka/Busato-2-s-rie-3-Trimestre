f57d20951724953f79022ccfceffa6ab9cc75b98
CrysParteka <162991031+CrysParteka@users.noreply.github.com>
2024-10-28 09:07:42 -0300

    b

--- app.js
+++ app.js
@@ -3,3 +3,15 @@ function criaCartao(categoria, pergunta, resposta) {
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
 }
+
+cartao.innerHTML = `
+<div class="cartao__conteudo">
+<h3>Programação</h3>
+<div class="cartao__conteudo__pergunta">
+        <p>O que é JavaScript?</p>
+</div>
+<div class="cartao__conteudo__resposta">
+        <p>O JavaScript é uma linguagem de programação</p>
+</div>
+</div>
+`
