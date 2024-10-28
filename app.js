158b4ec592cedaec03b87fac55534f0c88a9a728
CrysParteka <162991031+CrysParteka@users.noreply.github.com>
2024-10-28 09:26:55 -0300

    hhh

--- app.js
+++ app.js
@@ -5,17 +5,18 @@ function criaCartao(categoria, pergunta, resposta) {
 
 
 
-cartao.innerHTML = `
-<div class="cartao__conteudo">
-<h3>Programação</h3>
-<div class="cartao__conteudo__pergunta">
-        <p>O que é JavaScript?</p>
-</div>
-<div class="cartao__conteudo__resposta">
-        <p>O JavaScript é uma linguagem de programação</p>
-</div>
-</div>
-`
+    cartao.innerHTML = `
+    <div class="cartao__conteudo">
+    <h3>${categoria}</h3>
+    <div class="cartao__conteudo__pergunta">
+            <p>${pergunta}</p>
+    </div>
+    <div class="cartao__conteudo__resposta">
+            <p>${resposta}</p>
+    </div>
+    </div>
+    `
+    
 container.appendChild(cartao)
 }
 
