8b04ff16fc65053e6ada12f6ee9af3978c5f6da0
CrysParteka <162991031+CrysParteka@users.noreply.github.com>
2024-11-04 08:48:37 -0300

    xx

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ function criaCartao(categoria, pergunta, resposta) {
     </div>
     </div>
     `
+    let respostaEstaVisivel = false
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel
+    }
     
 container.appendChild(cartao)
 }
