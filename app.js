df6ac3f081a25bcabdada6b98494f58ce64cb527
CrysParteka <162991031+CrysParteka@users.noreply.github.com>
2024-11-04 08:49:55 -0300

    ddddd

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ function criaCartao(categoria, pergunta, resposta) {
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
 
-
     
     cartao.innerHTML = `
     <div class="cartao__conteudo">
