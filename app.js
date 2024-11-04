7a2b25d881c9d182e779ff96d027946dea06f247
CrysParteka <162991031+CrysParteka@users.noreply.github.com>
2024-11-04 09:00:29 -0300

    kkk

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ function criaCartao(categoria, pergunta, resposta) {
     let respostaEstaVisivel = false
     function viraCartao() {
         respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
     }
     
 container.appendChild(cartao)
